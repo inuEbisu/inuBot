18adc1940ace814a3a2e6044833f6023a4f4dc2a
inuEbisu_ <13098140@qq.com>
2022-02-18 18:04:26 +0800

    Update plugin 'random'

--- src/plugins/random/__init__.py
+++ src/plugins/random/__init__.py
@@ -1,9 +1,10 @@
 from nonebot import on_command
 from nonebot.typing import T_State
 from nonebot.adapters import Bot, Event
-from inukit import rand
+from ast import literal_eval as l_eval
+import random as r
 from . import lang
-matcher_random = on_command('random', priority=5)
+matcher_random = on_command('random', aliases={'r'}, priority=5)
 
 def is_int(i: object) -> bool:
     try:
@@ -19,21 +20,30 @@ def is_all_int(t: tuple) -> bool:
 async def main_random(bot: Bot, event: Event, state: T_State):
     args = str(event.get_message()).strip().split()
 
-    if len(args) == 0:
-        msg = str(rand.rand_a_int(0, 100))
-    elif len(args) == 1 and is_all_int(args):
-        ints = rand.rand_some_int(0, 100, int(args[0]))
-        tmp = [str(i) for i in ints]
-        msg = ' '.join(tmp)
-    elif len(args) == 2 and is_all_int(args):
-        msg = str(rand.rand_a_int(int(args[0]), int(args[1])))
-    elif len(args) == 3 and is_all_int(args):
-        ints = rand.rand_some_int(int(args[0]), int(args[1]), int(args[2]))
-        tmp = [str(i) for i in ints]
-        msg = ' '.join(tmp)
-    elif len(args) == 1 and args[0] == 'bool':
-        msg = str(rand.rand_bool())
-    else:
-        msg = lang.help_guide
-
+    try:
+        if len(args) == 0:
+            msg = str(r.randint(0, 100))
+        elif len(args) == 1 and is_all_int(args):
+            ints = [str(r.randint(0, 100)) for _ in range(0, int(args[0]))]
+            msg = ' '.join(ints)
+        elif len(args) == 2 and is_all_int(args):
+            msg = str(r.randint(int(args[0]), int(args[1])))
+        elif len(args) == 3 and is_all_int(args):
+            ints = [str(r.randint(int(args[0]), int(args[1]))) for _ in range(0, int(args[2]))]
+            msg = ' '.join(ints)
+        elif len(args) == 1 and (args[0] == 'bool' or args[0] == 'b'):
+            msg = str(r.choice((True, False)))
+        elif len(args) == 2 and (args[0] == 'shuffle' or args[0] == 's'):
+            lit = list(l_eval(args[1]))
+            r.shuffle(lit)
+            msg = str(lit)
+        elif len(args) == 2 and (args[0] == 'choice' or args[0] == 'c'):
+            msg = str(r.choice(l_eval(args[1])))
+        elif len(args) == 3 and (args[0] == 'choice' or args[0] == 'c') and is_int(args[2]):
+            msg = str(r.sample(l_eval(args[1]), k=int(args[2])))
+        else:
+            msg = lang.help_guide
+    except Exception as e:
+        msg = lang.err % (str(e),)
+    
     await matcher_random.finish(msg)
--- src/plugins/random/lang.py
+++ src/plugins/random/lang.py
@@ -1,6 +1,11 @@
-help_guide = '''/random help - 查阅帮助
+help_guide = '''命令和二级命令均可只写首字母
+/random help - 查阅帮助
 /random - 在 [0, 100] 间取一个随机数
 /random <数量> - 在 [0, 100] 间取若干个随机数
 /random <a> <b> - 在 [a, b] 间取一个随机数
 /random <a> <b> <数量> - 在 [a, b] 间取若干个随机数
-/random bool - 取随机布尔值(True or False)'''
+/random bool - 取随机布尔值(True or False)
+/random shuffle <序列> - 打乱该序列
+/random choice <序列> - 在序列中挑一个元素
+/random choice <序列> <数量> - 在序列中挑若干个元素'''
+err = '错误: %s'
